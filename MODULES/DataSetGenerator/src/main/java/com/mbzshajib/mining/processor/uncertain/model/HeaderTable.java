4693e2e4f459ea8f1ad8952990b6f72674618c57
Zaman Shajib <mbzshajib@gmail.com>
2015-09-30 12:03:17 +0600

    [TreeGeneration] - #Refactor# - Header table and its item updated.

--- MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/model/HeaderTable.java
+++ MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/model/HeaderTable.java
@@ -1,5 +1,7 @@
 package com.mbzshajib.mining.processor.uncertain.model;
 
+import com.mbzshajib.mining.exception.DataNotValidException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +18,45 @@ import java.util.List;
 
 
 public class HeaderTable {
+    private int windowSize;
     private List<HeaderTableItem> headerTableItems;
 
-    public HeaderTable() {
+    public HeaderTable(int windowSize) {
+        this.windowSize = windowSize;
         headerTableItems = new ArrayList<HeaderTableItem>();
     }
 
-    void updateHeaderTable(UNode uNode) {
+    public void updateHeaderTable(UNode uNode) throws DataNotValidException {
+        HeaderTableItem item = null;
+        if ((item = findHeaderTableItemById(uNode.getId())) == null) {
+            addNewTableItem(uNode.getId());
+            HeaderTableItem headerTableItem = findHeaderTableItemById(uNode.getId());
+            headerTableItem.updateHeaderData(uNode);
 
+        } else {
+            item.updateHeaderData(uNode);
+        }
+    }
+
+    private void addNewTableItem(String id) {
+        HeaderTableItem headerTableItem = new HeaderTableItem(id, windowSize);
+        headerTableItems.add(headerTableItem);
+    }
+
+    private HeaderTableItem findHeaderTableItemById(String id) {
+        HeaderTableItem result = null;
+        for (HeaderTableItem item : headerTableItems) {
+            if (item.getItemId().equalsIgnoreCase(id)) {
+                result = item;
+            }
+        }
+        return result;
+    }
+
+    public void slideWindowAndUpdateTable() {
+        for (HeaderTableItem item : headerTableItems) {
+            item.slideWindowAndUpdateItem();
+        }
     }
 
 }
--- MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/model/HeaderTableItem.java
+++ MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/model/HeaderTableItem.java
@@ -1,5 +1,10 @@
 package com.mbzshajib.mining.processor.uncertain.model;
 
+import com.mbzshajib.mining.exception.DataNotValidException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * *****************************************************************
  * Copyright  2015.
@@ -13,14 +18,73 @@ package com.mbzshajib.mining.processor.uncertain.model;
 
 
 public class HeaderTableItem {
-    private int groupSize;
     private String itemId;
-    private double[] prefixValues;
-    private double support;
+    private int windowSize;
+    private List<HeaderData> headerDataList;
 
-    public HeaderTableItem(int groupSize) {
-        this.groupSize = groupSize;
-        prefixValues = new double[groupSize];
+    public HeaderTableItem(String itemId, int windowSize) {
+        this.itemId = itemId;
+        this.windowSize = windowSize;
+        this.headerDataList = new ArrayList<HeaderData>();
+        for (int i = 0; i < windowSize; i++) {
+            HeaderData data = new HeaderData();
+            headerDataList.add(i, data);
+        }
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void updateHeaderData(UNode uNode) throws DataNotValidException {
+        if (windowSize < uNode.getFrameNo()) {
+            throw new DataNotValidException("Node Frame no is " + uNode.getFrameNo() + " it should less than window size " + windowSize);
+        }
+        HeaderData headerData = headerDataList.get(uNode.getFrameNo());
+        headerData.addNewNode(uNode);
+    }
+
+    public void slideWindowAndUpdateItem() {
+        HeaderData headerData = headerDataList.get(0);
+        headerDataList.remove(headerData);
+        headerDataList.add(new HeaderData());
+    }
+
+    public double getTotalSupport() {
+        double totalSupport = 0;
+        for (HeaderData headerData : headerDataList) {
+            for (UNode node : headerData.getNodes()) {
+                totalSupport += node.getItemProbability();
+            }
+        }
+        return totalSupport;
+    }
+
+    public double getTotalPrefixValue() {
+        double totalPrefixValue = 0;
+        for (HeaderData headerData : headerDataList) {
+            for (UNode node : headerData.getNodes()) {
+                totalPrefixValue += node.getPrefixValue();
+            }
+        }
+        return totalPrefixValue;
+    }
+
+
+    private class HeaderData {
+        List<UNode> nodes;
+
+        HeaderData() {
+            nodes = new ArrayList<UNode>();
+        }
+
+        void addNewNode(UNode uNode) {
+            nodes.add(uNode);
+        }
+
+        List<UNode> getNodes() {
+            return nodes;
+        }
     }
 
 }
