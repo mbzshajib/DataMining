a7db515f050293514e3a1623fef86b141e404e45
Zaman Shajib <mbzshajib@gmail.com>
2015-10-03 21:32:05 +0600

    [TreeGeneration] - #Minnig# - Mining processor input/output added.

--- MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/mining/UncertainStreamMineInput.java
+++ MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/mining/UncertainStreamMineInput.java
@@ -1,14 +1,18 @@
 package com.mbzshajib.mining.processor.uncertain.mining;
- /**
- * *****************************************************************
- * Copyright  2015.
- * @author - Md. Badi-Uz-Zaman Shajib
- * @email  - mbzshajib@gmail.com
- * @gitHub - https://github.com/mbzshajib
- * @date: 9/28/2015
- * @time: 7:46 PM
- * ****************************************************************
- */    
 
-public class UncertainStreamMining {
+import com.mbzshajib.utility.model.Input;
+
+/**
+ * *****************************************************************
+ *
+ * @author - Md. Badi-Uz-Zaman Shajib
+ * @copyright 2015.
+ * @email - mbzshajib@gmail.com
+ * @gitHub - https://github.com/mbzshajib
+ * @date: 10/3/2015
+ * @time: 9:30 PM
+ * ****************************************************************
+ */
+
+public class UncertainStreamMineInput implements Input{
 }
---
+++ MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/mining/UncertainStreamMiner.java
@@ -0,0 +1,23 @@
+package com.mbzshajib.mining.processor.uncertain.mining;
+
+import com.mbzshajib.utility.model.ProcessingError;
+import com.mbzshajib.utility.model.Processor;
+
+/**
+ * *****************************************************************
+ * Copyright  2015.
+ *
+ * @author - Md. Badi-Uz-Zaman Shajib
+ * @email - mbzshajib@gmail.com
+ * @gitHub - https://github.com/mbzshajib
+ * @date: 9/28/2015
+ * @time: 7:46 PM
+ * ****************************************************************
+ */
+
+public class UncertainStreamMiner implements Processor<UncertainStreamMineInput, UncertainStreamMineOutput> {
+    @Override
+    public UncertainStreamMineOutput process(UncertainStreamMineInput uncertainStreamMineInput) throws ProcessingError {
+        return null;
+    }
+}
