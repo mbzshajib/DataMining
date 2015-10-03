a78e380207b2807a48bbf669ec3b50241aa6bed1
Zaman Shajib <mbzshajib@gmail.com>
2015-10-04 00:45:16 +0600

    [Mining] - #CondTree# - Added new model for getting header info.

---
+++ MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/model/HTableItemInfo.java
@@ -0,0 +1,52 @@
+package com.mbzshajib.mining.processor.uncertain.model;
+
+/**
+ * *****************************************************************
+ *
+ * @author - Md. Badi-Uz-Zaman Shajib
+ * @copyright 2015.
+ * @email - mbzshajib@gmail.com
+ * @gitHub - https://github.com/mbzshajib
+ * @date: 10/3/2015
+ * @time: 9:53 PM
+ * ****************************************************************
+ */
+
+public class HTableItemInfo {
+    String itemId;
+    double itemProbabilityValue;
+    double itemPrefixValue;
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public double getItemProbabilityValue() {
+        return itemProbabilityValue;
+    }
+
+    public void setItemProbabilityValue(double itemProbabilityValue) {
+        this.itemProbabilityValue = itemProbabilityValue;
+    }
+
+    public double getItemPrefixValue() {
+        return itemPrefixValue;
+    }
+
+    public void setItemPrefixValue(double itemPrefixValue) {
+        this.itemPrefixValue = itemPrefixValue;
+    }
+
+    @Override
+    public String toString() {
+        return "HeaderInfo{" +
+                "itemId='" + itemId + '\'' +
+                ", Probability=" + itemProbabilityValue +
+                ", Prefix=" + itemPrefixValue +
+                '}';
+    }
+}
