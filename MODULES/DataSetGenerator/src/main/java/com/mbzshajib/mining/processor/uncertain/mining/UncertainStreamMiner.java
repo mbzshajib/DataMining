8ba0031e64ec88e6402ab30c9e485e5d69dc768e
Zaman Shajib <mbzshajib@gmail.com>
2015-10-04 00:49:08 +0600

    [Mining] - #CondTree# - new methods added to generate conditional tree.

--- MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/mining/UncertainStreamMiner.java
+++ MODULES/DataSetGenerator/src/main/java/com/mbzshajib/mining/processor/uncertain/mining/UncertainStreamMiner.java
@@ -1,10 +1,18 @@
 package com.mbzshajib.mining.processor.uncertain.mining;
 
+import com.mbzshajib.mining.processor.uncertain.model.HTableItemInfo;
+import com.mbzshajib.mining.processor.uncertain.model.HeaderTable;
+import com.mbzshajib.mining.processor.uncertain.model.UNode;
 import com.mbzshajib.mining.processor.uncertain.model.UncertainTree;
 import com.mbzshajib.mining.util.Constant;
 import com.mbzshajib.mining.util.Utils;
 import com.mbzshajib.utility.model.ProcessingError;
 import com.mbzshajib.utility.model.Processor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * *****************************************************************
@@ -23,12 +31,150 @@ public class UncertainStreamMiner implements Processor<UncertainStreamMineInput,
 
     @Override
     public UncertainStreamMineOutput process(UncertainStreamMineInput uncertainStreamMineInput) throws ProcessingError {
-        printBeforeMining(uncertainStreamMineInput);
+        UncertainTree uncertainTree = uncertainStreamMineInput.getUncertainTree();
+//        printBeforeMining(uncertainTree);
+        UNode rootNode = uncertainTree.getRootNode();
+        HeaderTable headerTable = uncertainTree.getHeaderTable();
+        double minSupport = uncertainStreamMineInput.getMinSupport();
+        List<HTableItemInfo> HTableItemInfoList = headerTable.getHeaderItemInfo();
+        removeDataBelowSupport(minSupport, HTableItemInfoList);
+        sortByPrefix(HTableItemInfoList);
+        startMining(rootNode, headerTable, HTableItemInfoList);
         return null;
     }
 
-    private void printBeforeMining(UncertainStreamMineInput uncertainStreamMineInput) {
-        UncertainTree uncertainTree = uncertainStreamMineInput.getUncertainTree();
+    private void startMining(UNode rootNode, HeaderTable headerTable, List<HTableItemInfo> HTableItemInfoList) {
+        mine(rootNode, "7");
+//        for (int index = HTableItemInfoList.size() - 1; index >= 0; index--) {
+//            HTableItemInfo HTableItemInfo = HTableItemInfoList.get(index);
+//            List<UNode> nodeList = headerTable.getAllNodesOfHeaderTableItem(HTableItemInfo.getItemId());
+//            mine(rootNode, "1");
+//
+//        }
+    }
+
+    private void mine(UNode rootNode, String id) {
+        //TODO: construct subTree;
+        UNode node = rootNode.copy();
+        System.out.println(node.traverse());
+        findNode(node, id);
+        System.out.println();
+        System.out.println(node.traverse());
+        //TODO: Mine SUbTree;
+    }
+
+    private UNode constructConditionalTree(UNode root, String id) {
+        int count = root.getChildNodeList().size();
+        for (int index = 0; index < count; index++) {
+            UNode child = root.getChildNodeList().get(index);
+            UNode foundNode = findNode(child, id);
+            if (foundNode == null) {
+                root.removeChildNode(child);
+                index--;
+                count--;
+            }
+        }
+        return root;
+    }
+
+    private UNode findNode(UNode node, String id) {
+        if (node.getId().equalsIgnoreCase(id)) {
+            node.setChildNodeList(new ArrayList<UNode>());
+            return node;
+        } else {
+            if (node.getChildNodeList() == null || node.getChildNodeList().isEmpty()) {
+                return null;
+            } else {
+                UNode returnNode = null;
+                int count = node.getChildNodeList().size();
+                for (int index = 0; index < count; index++) {
+                    UNode child = node.getChildNodeList().get(index);
+                    UNode foundNode = findNode(child, id);
+                    if (foundNode == null) {
+                        node.removeChildNode(child);
+                        index--;
+                        count--;
+                    } else {
+                        returnNode = foundNode;
+                    }
+                }
+                return returnNode;
+            }
+        }
+    }
+
+    private void removeDataBelowSupport(double minSupport, List<HTableItemInfo> HTableItemInfoList) {
+        removePrefixDataBelowSupport(HTableItemInfoList, minSupport);
+        removeProbabilityDataBelowSupport(HTableItemInfoList, minSupport);
+    }
+
+    private void removePrefixDataBelowSupport(List<HTableItemInfo> HTableItemInfoList, double minSupport) {
+        for (int index = 0; index < HTableItemInfoList.size(); index++) {
+            HTableItemInfo HTableItemInfo = HTableItemInfoList.get(index);
+            if (HTableItemInfo.getItemPrefixValue() < minSupport) {
+                HTableItemInfoList.remove(HTableItemInfo);
+            }
+        }
+    }
+
+    private void removeProbabilityDataBelowSupport(List<HTableItemInfo> HTableItemInfoList, double minSupport) {
+        for (int index = 0; index < HTableItemInfoList.size(); index++) {
+            HTableItemInfo HTableItemInfo = HTableItemInfoList.get(index);
+            if (HTableItemInfo.getItemProbabilityValue() < minSupport) {
+                HTableItemInfoList.remove(HTableItemInfo);
+            }
+        }
+    }
+
+    private void sortByID(List<HTableItemInfo> sortedFilteredById) {
+        Collections.sort(sortedFilteredById, new Comparator<HTableItemInfo>() {
+            @Override
+            public int compare(HTableItemInfo one, HTableItemInfo two) {
+                int idOne = Integer.parseInt(one.getItemId());
+                int idTwo = Integer.parseInt(two.getItemId());
+                int diff = idTwo - idOne;
+                return diff;
+            }
+        });
+
+    }
+
+    private void sortByPrefix(List<HTableItemInfo> sortedFilteredById) {
+        Collections.sort(sortedFilteredById, new Comparator<HTableItemInfo>() {
+            @Override
+            public int compare(HTableItemInfo one, HTableItemInfo two) {
+                double diff = two.getItemPrefixValue() - one.getItemPrefixValue();
+                if (diff == 0) {
+                    return 0;
+                } else if (diff > 0) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+    }
+
+    private void sortByProbability(List<HTableItemInfo> sortedFilteredById) {
+        Collections.sort(sortedFilteredById, new Comparator<HTableItemInfo>() {
+            @Override
+            public int compare(HTableItemInfo one, HTableItemInfo two) {
+                double diff = two.getItemProbabilityValue() - one.getItemProbabilityValue();
+                if (diff == 0) {
+                    return 0;
+                } else if (diff > 0) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+    }
+
+    private void printBeforeMining(UncertainTree uncertainTree) {
+
         Utils.log(Constant.MULTI_STAR);
         Utils.log(TAG, "Tree For Mining");
         Utils.log(Constant.MULTI_STAR);
