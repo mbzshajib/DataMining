101a3ebf0ea8911c71b060a0c675864677b96916
Zaman Shajib <mbzshajib@gmail.com>
2015-10-06 14:10:49 +0600

    [DataSet] - #Configuration#  new data not found exception added.

---
+++ MODULES/Utility/src/main/java/com/mbzshajib/utility/exception/DataNotFoundException.java
@@ -0,0 +1,20 @@
+package com.mbzshajib.utility.exception;
+
+/**
+ * *****************************************************************
+ * Copyright  2015.
+ *
+ * @author - Md. Badi-Uz-Zaman Shajib
+ * @email - mbzshajib@gmail.com
+ * @gitHub - https://github.com/mbzshajib
+ * @date: 10/6/2015
+ * @time: 12:39 PM
+ * ****************************************************************
+ */
+
+
+public class DataNotFoundException extends Throwable {
+    public DataNotFoundException(String s) {
+        super(s);
+    }
+}
