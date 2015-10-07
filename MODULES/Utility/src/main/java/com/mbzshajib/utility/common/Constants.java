37d9df9126ae421df72dc2ce768ef9f571c31896
Zaman Shajib <mbzshajib@gmail.com>
2015-10-07 13:05:34 +0600

    [DataSet] - #Constant#  GSON file extension added to constant.

--- MODULES/Utility/src/main/java/com/mbzshajib/utility/common/Constants.java
+++ MODULES/Utility/src/main/java/com/mbzshajib/utility/common/Constants.java
@@ -1,14 +1,18 @@
-package com.mbzshajib.utility.common; /**
+package com.mbzshajib.utility.common;
+
+/**
  * *****************************************************************
  * Copyright  2015.
+ *
  * @author - Md. Badi-Uz-Zaman Shajib
- * @email  - mbzshajib@gmail.com
+ * @email - mbzshajib@gmail.com
  * @gitHub - https://github.com/mbzshajib
  * @date: 9/4/2015
  * @time: 4:32 PM
  * ****************************************************************
  */
-     
+
 
 public class Constants {
+    public static final String FILE_EXT_JSON = ".json";
 }
