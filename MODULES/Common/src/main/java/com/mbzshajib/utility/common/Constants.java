1d7028ce025c5d2aa642ed19e56036d99a790180
Zaman Shajib <mbzshajib@gmail.com>
2015-10-10 23:11:58 +0600

    [Simulation] - new constant added.

--- MODULES/Common/src/main/java/com/mbzshajib/utility/common/Constants.java
+++ MODULES/Common/src/main/java/com/mbzshajib/utility/common/Constants.java
@@ -23,6 +23,8 @@ public class Constants {
     public static final String NEW_LINE = "\n";
     public static final String TAB = "\t";
     public static final String STAR = "*";
+    public static final String UNDER_SCORE = "_";
+    public static final String HIGH_PHEN = "-";
     public static final String HASH = "#";
     public static final String TABBED_HASH = "\t#";
     public static final String MULTI_STAR = "******************************************************************************************************************************************************************";
